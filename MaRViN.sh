c1200b02721b1357ecdef53c6ac8455cf36e222f
Giacomo Fulvi <giacomofulvi@MBP-Summer6.local>
2024-03-15 10:10:06 +0100

    modded sh script

--- MaRViN.sh
+++ MaRViN.sh
@@ -9,93 +9,105 @@ CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 RESET='\033[0m'
 
+UKNOWN="[????]"
+MARVIN="[MaRViN]"
+
 clear
 
-str="[???]: Saluti umano, io sono MaRViN. Il mio creatore mi ha programmato per aiutare creature come te a navigare la complessa struttura del nostro alveare."
-
-while IFS= read -r -n1 var
-do
-	printf '%s' "$var"
-	sleep 0.015
-done <<< "$str"
-
-echo
-sleep 0.3
-echo
-
-str="[MaRViN]: Il mio creatore sta cercando un nuovo assistente umano per il suo laboratorio, pertanto le tue abilità verranno testate ora... Se ti stai chiedendo se puoi rifiutare, la risposta è no, non puoi rifiutare."
-while IFS= read -r -n1 var
-do
-	printf '%s' "$var"
-	sleep 0.015
-done <<< "$str"
-
-echo
-sleep 0.3
-echo
-
-str="[MaRViN]: Il test a cui sarai sottoposto consiste in una caccia al tesoro: dovrai trovare la chiave del laboratorio. Avrai bisogno di tutte le conoscenze che hai acquisito fino ad ora, e forse anche di qualcuna in più :)"
-
-while IFS= read -r -n1 var
-do
-	printf '%s' "$var"
-	sleep 0.015
-done <<< "$str"
-
-echo
-sleep 0.3
-echo
-
-str="[MaRViN]: Per trovare la chiave, dovrai chiedere e cercare indizi dai vari membri dell'alveare."
-
-while IFS= read -r -n1 var
-do
-	printf '%s' "$var"
-	sleep 0.015
-done <<< "$str"
-
-echo
-sleep 0.3
-echo
-
-str="[MaRViN]: Prima di iniziare con il test effettivo, suggerirei di fare un piccolo riscaldamento. Ti chiedo quindi di andare nella cartella chiamata 'practice', creare una sottocartella chiamata 'MaRViN_stuff' e eliminare un'altra cartella già presente al suo interno chiamata 'MaRViN_secrets'."
-
-while IFS= read -r -n1 var
-do
-	printf '%s' "$var"
-	sleep 0.015
-done <<< "$str"
-
-echo
-sleep 0.3
-echo
-
 print_animated() {
-    str="$1"
-    while IFS= read -r -n1 var; do
+    local message=$1
+	local color=$2
+	printf "%b" "$color"
+    while IFS= read -r -n1 var;
+	do
         printf '%s' "$var"
         sleep 0.015
-    done <<< "$str"
-    echo
-	echo
+    done <<< "$message"
+	printf "%b" "$RESET"
 }
 
-print_animated "[MaRViN]: Una volta completato, dovrai eseguire lo script solution.sh in questo modo: "
+print_animated "$UKNOWN" "$RED"
 
-printf "${GREEN}sh solution.sh"
-printf "${RESET}"
-echo
-sleep 0.3
-echo
-
-str="[MaRViN]: Ricorda che dovrai ripetere la stessa cosa in tutti i livelli per verificare le soluzioni."
-
-while IFS= read -r -n1 var
-do
-	printf '%s' "$var"
-	sleep 0.015
-done <<< "$str"
+str=": Saluti umano, io sono MaRViN. Il mio creatore mi ha programmato per aiutare creature come te a navigare la complessa struttura del nostro alveare. Il mio creatore sta cercando un nuovo assistente umano per il suo laboratorio, pertanto le tue abilità adesso verranno testate... Se ti stai chiedendo se puoi rifiutarti la risposta è no, non puoi rifiutarti."
+print_animated "$str" "$WHITE"
 
 echo
-sleep 0.3
+sleep 0.5
+echo
+
+print_animated "$MARVIN" "$GREEN"
+
+str=": Il test al quale sarai sottoposto consiste in una caccia al tesoro, dovrai trovare la chiave del laboratorio. Avrai bisogno di tutte le conoscenze che hai acquisito fino ad ora, e forse qualcuna in più :)"
+print_animated "$str" "$WHITE"
+
+echo
+sleep 0.5
+echo
+
+print_animated "$MARVIN" "$GREEN"
+
+str=": Per trovare la chiave dovrai chiedere e cercare indizi dai vari membri dell'alveare."
+print_animated "$str" "$WHITE"
+
+echo
+sleep 0.5
+echo
+
+print_animated "$MARVIN" "$GREEN"
+
+str=": Prima di cominciare con l'effettivo test, direi di fare un piccolo riscaldamento. Ti chiedo quindi di andare nella cartella chiamata " 
+print_animated "$str" "$WHITE"
+
+print_animated "practice" "$YELLOW"
+
+str=", crearvi una cartella chiamata "
+print_animated "$str" "$WHITE"
+
+print_animated "MaRViN_stuff" "$YELLOW"
+
+ 
+str=" e di eliminarne un'altra già al suo interno chiamata "
+print_animated "$str" "$WHITE"
+
+print_animated "MaRViN_secrets" "$YELLOW"
+
+printf "."
+
+echo
+sleep 0.5
+echo
+
+print_animated "$MARVIN" "$GREEN"
+
+str=": Una volta fatto ciò dovrai lanciare lo script solution.sh in questo modo: "
+print_animated "$str" "$WHITE"
+
+echo
+sleep 0.5
+echo
+
+solution="sh solution.sh"
+
+print_animated "$solution" "$PURPLE"
+
+echo
+sleep 0.5
+echo
+
+print_animated "$MARVIN" "$GREEN"
+
+str=": Ricorda che questo comando lo dovrai ripetere in tutti i livelli per verificarne le soluzioni."
+print_animated "$str" "$WHITE"
+
+echo
+sleep 0.5
+echo
+
+print_animated "$MARVIN" "$GREEN"
+
+str=": Good luck! Have fun! Dont't die!"
+print_animated "$str" "$WHITE"
+
+echo
+sleep 0.5
 echo
